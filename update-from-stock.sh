6584b8e700102f7e1bb07737123d070b2f4a9484
void(0) <munchycool@users.noreply.github.com>
2018-05-10 15:06:11 -0400

    update gapps from extracted factory image.
    Usage is ./update-from-stock.sh path_to_extracted_factory_image

--- update-from-stock.sh
+++ update-from-stock.sh
@@ -1,43 +1,39 @@
 #/bin/sh
-
 apps="$(ls app/)"
 privs="$(ls priv-app/)"
 fws="$(ls framework/)"
 libs="$(ls lib/)"
 lib64s="$(ls lib64/)"
-
+src=$1
 cd app;
 for a in $apps; do
     if [[ "$a" != "Android.mk" ]]; then
-    cp -R ../../taimen/system/app/"$a" .;
-    cp -R ../../taimen/data/app/"$a" .;
+    cp -R $src/system/app/"$a" .;
+#    cp -R ../../taimen/data/app/"$a" .;
     fi;
 done;
 cd ../priv-app;
 for p in $privs; do
     if [[ "$p" != "Android.mk" ]]; then
-    cp -R ../../taimen/system/priv-app/"$p" .;
-    cp -R ../../taimen/data/priv-app/"$p" .;
+    cp -R $src/system/priv-app/"$p" .;
+#    cp -R ../../taimen/data/priv-app/"$p" .;
     fi;
 done;
 cd ../framework;
 for f in $fws; do
     if [[ "$f" != "Android.mk" ]]; then
-    cp -R ../../taimen/system/framework/"$f" .;
+    cp -R ~$src/system/framework/"$f" .;
     fi;
 done;
 cd ../lib;
 for l in $libs; do
     if [[ "$l" != "Android.mk" ]]; then
-    cp -R ../../taimen/system/lib/"$l" .;
+    cp -R $src/system/lib/"$l" .;
     fi;
 done;
 cd ../lib64;
-for l64 in $lib64s; do
+or l64 in $lib64s; do
     if [[ "$l64" != "Android.mk" ]]; then
-    cp -R ../../taimen/system/lib64/"$l64" .;
+    cp -R $src/system/lib64/"$l64" .;
     fi;
 done;
-
-
-
