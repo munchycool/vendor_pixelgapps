7114dbb88f13abfd81462646be7192083b9c1d2e
munchycool <munchy.cool@gmail.com>
2018-05-11 17:06:02 +0000

    add usage

--- check_apk_package_name.sh
+++ check_apk_package_name.sh
@@ -1,3 +1,7 @@
+#/bin/sh
+
+#usage is  ./check_apk_package_name.sh path_to_apk
+
 package=$(aapt dump badging "$*" | awk '/package/{gsub("name=|'"'"'","");  print $2}')
 activity=$(aapt dump badging "$*" | awk '/activity/{gsub("name=|'"'"'","");  print $2}')    echo
 echo "   file : $1"
