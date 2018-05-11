b4e7b31795e4e6113a1680303331956754ef877b
munchycool <munchy.cool@gmail.com>
2018-05-11 12:06:21 +0000

    Revert "Remove AndroidPlatformServices-no longer in Factory Image"
    This reverts commit 2ede6285db33e2217d972a8c8dca447c8f4e5bbe.

--- priv-app/Android.mk
+++ priv-app/Android.mk
@@ -14,15 +14,15 @@
 
 LOCAL_PATH := $(call my-dir)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := AndroidPlatformServices
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_SRC_FILES := AndroidPlatformServices/AndroidPlatformServices.apk
-#LOCAL_CERTIFICATE := PRESIGNED
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_PRIVILEGED_MODULE := true
-#LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := AndroidPlatformServices
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := AndroidPlatformServices/AndroidPlatformServices.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := AndroidMigratePrebuilt
